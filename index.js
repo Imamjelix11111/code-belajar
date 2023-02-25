b23f3fd2e7b09325f1562013d8899ac005401284
Imamjelix11111 <125793778+Imamjelix11111@users.noreply.github.com>
2023-02-25 23:47:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
+// import class obj
+
 const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
 const db = require('./connections')
 const response = require('./response')
+const path = require('path')
 
+
+
+// set
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+//user
+app.use('/styles', express.static("public"))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/listdata', (req, res) => {
-  const sql = `SELECT * FROM brother`
 
-  db.query(sql, (error, result) => {
-  response(200, "nih data", res) 
+// backend ops
+
+// path/
+app.get('/', (req, res)=> {
+  res.render('home.ejs')
+})
+
+
+
+//path register
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+  const  {username, password, email }= req.body
+  const sql = `INSERT INTO login (username, password, email) VALUES ('${username}', '${password}', '${email}') `
+  db.query(sql, (err, result)=> {
+    console.log(result)
+    if (err) response(500, "error pak", res)
   })
-})
-
-app.get('/brother', (req, res) => {
-  const brother = `SELECT * FROM brother`
-  db.query(brother, (err, fields) => {
-    if (err) throw err
-    response(200, fields, res)
-  })
-})
-app.get('/brother/:Nomor', (req,res) => {
-  const Nomor = req.params.Nomor
-  const sql = `SELECT Nomor FROM brother WHERE Nomor = ${Nomor}`
-  db.query(sql, (err, fields) => {
-    response (200, fields, res)
-  })
-})
-app.get('/find', (req, res) =>  {
-  const find = `SELECT Nama_Lengkap FROM brother WHERE Nama_Lengkap = ${req.query.Kelas}`
- 
-  db.query(find, (error, result) => {
-    response(200, "find data brother", res)
-  })
-})
-
-app.get('/blog', (req, res) => {
-  response(200, 'Ini', res)
+  res.render('home.ejs')
 })
 
 
-app.get('/resume', (req,res) =>  {
-  response(200, 'Ini Resume',  res)
+
+// path blog
+app.get('/Blog', (req,res) =>  {
+  res.send('ini timeline')
 })
 
 
-app.post('/login', (req,res) => {
-    console.log(req.body)
-    response(200, 'login berhasil', res)
+
+
+// path timeline
+app.get('/Timeline', (req,res) =>  {
+  res.send('ini timeline')
 })
 
-app.post('/regist', (req,res) => {
+
+
+
+// path quotes
+app.get('/Quotes', (req,res) => {
   console.log(req.body)
-  response(200, 'form regist', res)
+  res.send('ini quotes')
 })
 
 
-app.delete('/', (req,res) => {
-  response(200,'update berhasil',res)
+
+//path about
+app.get('/About', (req,res) => {
+  console.log(req.body)
+  res.send('ini quotes')
+  
 })
 
+
+
+// jalankan backend
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
